bebac47a2487d27550b1071f34f0a5a58970ec82
Ben Costas <62489019+bencostas@users.noreply.github.com>
2021-10-16 16:23:25 -0400

    figuring out routes in landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import FilterPhase from './components/FilterPhase/App';
+//import FilterPhase from './components/FilterPhase/App';
 import LandingPage from './components/LandingPage';
+
 
 function App() {
   return (
     <div>
       <LandingPage/>
-      <FilterPhase/>
     </div>
   );
 }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,20 +1,29 @@
 import logo from '../foodfind-logo.png';
 import './LandingPage.css';
-import FilterPhase from './FilterPhase';
+import FilterPhase from './FilterPhase/App';
+import {
+  BrowserRouter as Router, Route, Switch, Link, BrowserRouter
+} from 'react-router-dom';
+
 
 function LandingPage() {
+
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo}  alt="logo" className="logo"/> 
         <p>
         Decide what you want to eat with your friends!
         </p>
-        <button className="startButton" onclick={FilterPhase}>
-          Start Search
+        <button>
         </button>
+          <Link path='./FilterPhase' component={FilterPhase}>
+            Start Search
+          </Link>
       </header> 
     </div>
+    </Router>
   );
 }
 
